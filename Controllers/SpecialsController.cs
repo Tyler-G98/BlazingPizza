ec32bd2238e4c6c79090479cd17adad169ae47d4
Tyler G <tgartner062698@gmail.com>
2025-09-16 23:08:50 -0500

    added some db context

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Controllers;
+
+//base URL for this controller
+/*If app runs at https://localhost:5001, the full URL for this 
+ * controller will be https://localhost:5001/specials */
+[Route("specials")]
+
+//Dedicate to being an API controller
+/* Gives automatic features:
+ *  model validation/binding
+ *  Returns 400 Bad Request if input is invalid
+ *  Helps format responses as JSON or XML
+ */
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]//Responds to GET requests to /specials
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+}
